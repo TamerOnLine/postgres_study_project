7e6d63cbd7d577a17f3e84b39e8577c879cfa165
TamerOnLine <tamer@tameronline.com>
2025-06-16 00:19:04 +0200

    manu

--- tools/db/postgres/main.py
+++ tools/db/postgres/main.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
+
+
 try:
     import setup_path  # يسمح بتشغيل الملف مباشرة
 except ImportError:
